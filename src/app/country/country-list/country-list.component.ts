5a450867bda2aeb7d6e7068f9ea59da45eebc7b3
nehais <nehasinha82@gmail.com>
2024-05-21 20:31:56 +0200

    UN Member status in readable value & Unsubscribe

--- src/app/country/country-list/country-list.component.ts
+++ src/app/country/country-list/country-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CountryService } from '../country.service';
 import { Country } from 'src/app/models/country';
-import { Subject, Subscription, of } from 'rxjs';
+import { Subject, Subscription, of, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-country-list',
@@ -13,25 +13,26 @@ export class CountryListComponent implements OnInit, OnDestroy{
   countries: Country[] = [];
   countryDetails : Country[] =[];
   countrySelect : string | undefined;
-  displayedColumns: string[] = ['name', 'code', 'unMember','population', 'languages', 'flag'];
+  displayedColumns: string[] = ['name', 'code', 'unMember','population', 'languages', 'flags'];
   selectError : boolean = false;
-  //unsubscribe$ : new Subject<>();
+  unsubscribe$ = new Subject<void>();
 
   constructor (private countryService: CountryService){}
 
   ngOnInit(): void {
-    this.countryService.getAllCountries().subscribe(data =>{
+    this.countryService.getAllCountries().pipe(takeUntil(this.unsubscribe$)).subscribe(data =>{
       this.countries = data;
     });
   }
 
   ngOnDestroy(): void{
-    
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   getCountryDetails(){
     if (this.countrySelect){
-      this.countryService.getCountryDetailsWithCode(this.countrySelect).subscribe(data =>{
+      this.countryService.getCountryDetailsWithCode(this.countrySelect).pipe(takeUntil(this.unsubscribe$)).subscribe(data =>{
         this.countryDetails = data;
       })
     }
